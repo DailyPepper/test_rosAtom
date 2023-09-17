b91bca26bf6c988bf0ec8a05fa578ec1076954d7
DailyPepper <danielkudinov17@gmail.com>
2023-09-18 00:21:42 +0300

    start-end work

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-const tabsLabel = document.querySelectorAll(".tabs__label");
-const tabsBlock = document.querySelectorAll(".tabs-block");
 
-function toggleTabs(index) {
-  for (let i = 0; i < tabsBlock.length; i++) {
-     tabsBlock[i].classList.remove("tabs__block_active");
-    if (index === i) {
-      tabsBlock[index].classList.add("tabs__block_active");
-    }
-  }
-}
-
-const tabsContainer = document.querySelector(".tabs");
-
-tabsContainer.addEventListener("click", (event) => {
-  const targetTab = event.target.closest(".tabs__label");
-  if (targetTab) {
-    const index = Array.from(tabsLabel).indexOf(targetTab);
-    toggleTabs(index);
-  }
-});
- 
 const form = document.querySelector(".list__form");
 const formInput = document.querySelector(".list__input");
 const listItems = document.querySelector(".list-items");
