2b2a64ffce678219ac2c7f28816fed645ec34fa1
DailyPepper <danielkudinov17@gmail.com>
2023-09-19 00:21:51 +0300

    js/main and add style

--- js/main.js
+++ js/main.js
@@ -1,32 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".list__form");
+  const formInput = document.querySelector(".list__input");
+  const listItems = document.querySelector(".list__items");
+  const listCount = document.querySelector(".list__cnt");
 
-const form = document.querySelector(".list__form");
-const formInput = document.querySelector(".list__input");
-const listItems = document.querySelector(".list-items");
-const listCount = document.querySelector(".list__cnt");
+  let cnt = 1;
 
-let cnt = 1;
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addElem();
+  });
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addElem();
-});
+  listItems.addEventListener("click", function (e) {
+    if (e.target.classList.contains("list__items-button")) {
+      deleteElem(e.target.parentElement);
+    } else if (e.target.classList.contains("list__item-button")) {
+      completeElem(e.target.parentElement);
+    }
+  });
 
-listItems.addEventListener("click", delElem);
-function addElem() {
-  const formElem = formInput.value;
-  if (formElem === "") return;
-  const listItem = document.createElement("li");
-  listItem.classList.add("list-items__item");
-  listItem.innerHTML = `<p class="list-items__text">${formElem} </p> <button class="list-item__button">Удалить</button>`;
-  listItems.appendChild(listItem);
-  formInput.value = "";
-  listCount.textContent = ++cnt;
-}
+  function addElem() {
+    const formElem = formInput.value.trim();
+    if (formElem === "") return;
 
-function delElem(e) {
-  if (e.target.classList.contains("list-item__button")) {
-    e.target.closest(".list-items__item").remove();
+    const listItem = document.createElement("li");
+    listItem.classList.add("list__item-block");
+    listItem.innerHTML = `
+      <p class="list-items__text">${formElem}</p>
+      <button class="list__item-button">complete</button>
+      <button class="list__items-button">delete</button>
+    `;
+
+    listItems.insertBefore(listItem, listItems.firstChild);
+
+    formInput.value = "";
+    listCount.textContent = ++cnt;
+  }
+
+  function deleteElem(elem) {
+    elem.remove();
     cnt--;
     listCount.textContent = cnt;
   }
-}
+
+  function completeElem(elem) {
+    elem.classList.toggle("completed");
+    listItems.insertBefore(elem, listItems.end);
+  }
+});
+
+$('.switch-btn').click(function(){
+  $(this).toggleClass('switch-on');
+  if ($(this).hasClass('switch-on')) {
+    $(this).trigger('on.switch');
+  } else {
+    $(this).trigger('off.switch');
+  }
+});
